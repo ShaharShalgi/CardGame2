0a5ebf3a949a18953840bea2dd1ede8e587cda25
User <User@LAB3-3>
2023-09-06 11:26:38 +0300

    update game

--- CardGame/Deck.cs
+++ CardGame/Deck.cs
@@ -9,12 +9,34 @@ namespace CardGame
     public class Deck
     {
         private Card[] cards;
+        private int top;
 
-        public Deck() { }
+        public Deck() 
+        {
+             top = 0;
+            Card[] cards = new Card[52];
+            
+            
+            int index = 0;
+            for (int i = 1;  i <= 4; i++)
+            {
+                for(int j = 1; j <= 13; j++)
+                {
+                    cards[index] = new Card(j, i);
+                        index++;
+
+                }
+            }
+        }
         public Card Peek() { return null; }
         public void Shuffle() { }
-        public Card Deal() { return null; }
+        public Card Deal() 
+        {
+            Card c = cards[top];
+            top++;
+            return c;
+        }
 
-        public void Reset() { }
+        public void Reset() { top = 0; }
     }
 }
